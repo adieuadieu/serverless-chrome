a873012846160e7b03cef088472b5c86be352f16
Marco Lüthy <marco@luethy.net>
2017-05-03 16:55:21 +0800

    feat: add printToPdf handler
    Latest builds of headless Chrome include support for Chrome Debugging Protocol's printToPDF() method. Adding an exmaple handler using said method.

---
+++ src/handlers/printToPdf.js
@@ -0,0 +1,89 @@
+import Cdp from 'chrome-remote-interface'
+import config from '../config'
+import { log, sleep } from '../utils'
+
+export async function printUrlToPdf (url) {
+  const LOAD_TIMEOUT = (config && config.chrome.pageLoadTimeout) || 1000 * 60
+
+  let result
+  let loaded = false
+
+  const loading = async (startTime = Date.now()) => {
+    if (!loaded && Date.now() - startTime < LOAD_TIMEOUT) {
+      await sleep(100)
+      await loading(startTime)
+    }
+  }
+
+  const tab = await Cdp.New({ host: '127.0.0.1' })
+  const client = await Cdp({ host: '127.0.0.1', tab })
+
+  const { Network, Page } = client
+
+  Network.requestWillBeSent((params) => {
+    log('Chrome is sending request for:', params.request.url)
+  })
+
+  Page.loadEventFired(() => {
+    loaded = true
+  })
+
+  if (config.logging) {
+    Cdp.Version((err, info) => {
+      console.log('CDP version info', err, info)
+    })
+  }
+
+  try {
+    await Network.enable() // https://chromedevtools.github.io/debugger-protocol-viewer/tot/Network/#method-enable
+    await Page.enable() // https://chromedevtools.github.io/debugger-protocol-viewer/tot/Page/#method-enable
+    await Page.navigate({ url }) // https://chromedevtools.github.io/debugger-protocol-viewer/tot/Page/#method-navigate
+    await loading()
+
+    // https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-printToPDF
+    const pdf = await Page.printToPDF({
+      printBackgrounds: true,
+      marginTop: 0,
+      marginBottom: 0,
+      marginLeft: 0,
+      marginRight: 0,
+      paperType: 'A4',
+    })
+    result = pdf.data
+  } catch (error) {
+    console.error(error)
+  }
+
+  await client.close()
+
+  return result
+}
+
+export default (async function printToPdfHandler (event) {
+  const { queryStringParameters: { url } } = event
+  let pdf
+
+  log('Processing PDFification for', url)
+
+  try {
+    pdf = await printUrlToPdf(url)
+  } catch (error) {
+    console.error('Error printing pdf for', url, error)
+    throw new Error('Unable to print pdf')
+  }
+
+  return {
+    statusCode: 200,
+    // it's not possible to send binary via AWS API Gateway as it expects JSON response from Lambda
+    body: `
+      <html>
+        <body>
+          <embed src="data:application/pdf;base64,${pdf}" width="100%" height="100%" type='application/pdf'>
+        </body>
+      </html>
+    `,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  }
+})
