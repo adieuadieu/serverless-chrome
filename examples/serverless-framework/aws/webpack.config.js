0ce683ea82aa1cc1e73b2cbb6e06da4204cd6a4d
will Farrell <will.farrell@gmail.com>
2017-07-10 09:41:51 -0600

    Add in webpack compiling

---
+++ examples/serverless-framework/aws/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    captureScreenshotHtml:'./src/captureScreenshotHtml',
+    captureScreenshotBinary:'./src/captureScreenshotBinary',
+    experimental:'./src/experimental',
+    printToPdfHtml:'./src/printToPdfHtml',
+    printToPdfBinary:'./src/printToPdfBinary',
+    requestLogger:'./src/requestLogger',
+    versionInfo:'./src/versionInfo',
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+    ],
+  },
+  resolve: {
+
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: __dirname+'/.webpack',
+    filename: '[name].js'
+  },
+  externals: ['aws-sdk'],
+  plugins: [
+  ],
+}
