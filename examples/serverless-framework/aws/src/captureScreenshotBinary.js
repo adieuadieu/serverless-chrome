ef421bb36d59c039d7511c5c798702a96ee63204
will Farrell <will.farrell@gmail.com>
2017-08-01 18:07:53 -0600

    Change handler name

--- examples/serverless-framework/aws/src/captureScreenshotBinary.js
+++ examples/serverless-framework/aws/src/captureScreenshotBinary.js
@@ -2,7 +2,7 @@ const { log } = require('./utils');
 
 const captureScreenshotOfUrl = require('./captureScreenshot');
 
-exports.captureScreenshotBinaryHandler = function(event) {
+exports.handler = function(event) {
   const url = event.queryStringParameters.url;
 
   log('Processing screenshot capture for', url);
