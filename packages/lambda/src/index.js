dd161fd97d3f7c1e7b2cf6754e1ed18add3e2e3a
Mark S. Everitt <mark.s.everitt@gmail.com>
2017-08-29 09:44:01 +0100

    Updates postinstall script and module to include NSS build.

--- packages/lambda/src/index.js
+++ packages/lambda/src/index.js
@@ -1,10 +1,20 @@
 import fs from 'fs'
+import path from 'path'
 import LambdaChromeLauncher from './launcher'
 import { debug } from './utils'
 import DEFAULT_CHROME_FLAGS from './flags'
 
 const DEVTOOLS_PORT = 9222
 const DEVTOOLS_HOST = 'http://127.0.0.1'
+
+// Prepend NSS related libraries and binaries to the library path and path respectively on lambda.
+if (process.env.AWS_EXECUTION_ENV) {
+  const nssSubPath = fs.readFileSync(path.join(__dirname, 'nss', 'latest'), 'utf8').trim();
+  const nssPath = path.join(__dirname, 'nss', subnssSubPathPath);
+
+  process.env.LD_LIBRARY_PATH = path.join(nssPath, 'lib') +  ':' + process.env.LD_LIBRARY_PATH;
+  process.env.PATH = path.join(nssPath, 'bin') + ':' + process.env.PATH;
+}
 
 // persist the instance across invocations
 // when the *lambda* container is reused.
