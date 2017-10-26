7bca8febbc7cfc695b14ac0624634316e3c9d5b7
Marco Lüthy <marco@luethy.net>
2017-10-26 13:33:42 +0200

    circleci 2

--- scripts/update-docker-image-base.sh
+++ scripts/update-docker-image-base.sh
@@ -14,10 +14,14 @@ set -e
 cd "$(dirname "$0")/.."
 
 PROJECT_DIRECTORY=$(pwd)
-
 PACKAGE_DIRECTORY="$PROJECT_DIRECTORY/packages/lambda"
 
 cd "$PACKAGE_DIRECTORY"
+
+# ref: https://stackoverflow.com/a/39731444/845713
+docker_tag_exists() {
+    curl --silent -f -lSL "https://index.docker.io/v1/repositories/$1/tags/$2" > /dev/null
+}
 
 build() {
   BUILD_NAME=$1
@@ -25,10 +29,11 @@ build() {
   cd "$PACKAGE_DIRECTORY/builds/$BUILD_NAME"
   
   DOCKER_IMAGE=$BUILD_NAME-for-amazonlinux-base
-  CURRENT_VERSION=$(jq -r ".stable" version.json)
   LATEST_VERSION=$(./latest.sh)
 
-  if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
+  if docker_tag_exists "adieuadieu/$DOCKER_IMAGE" "$LATEST_VERSION"; then
+    echo "$BUILD_NAME version $LATEST_VERSION was previously built. Skipping build."
+  else
     echo "Building Docker image $BUILD_NAME version $LATEST_VERSION"
     
     docker build \
@@ -38,18 +43,20 @@ build() {
       --build-arg VERSION="$LATEST_VERSION" \
       base/
     
-    if [ ! -z "$DOCKER_USER" ] && [ ! -z "$DOCKER_PASS" ]; then
       echo "Pushing image to Docker hub"
-
-      docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
       docker push "adieuadieu/chromium-for-amazonlinux-base:$LATEST_VERSION"
-    fi  
-  else
-    echo "$BUILD_NAME version $LATEST_VERSION was previously built. Skipping build."
   fi
 }
 
 # main script
+
+if [ -z "$DOCKER_USER" ] || [ -z "$DOCKER_PASS" ]; then
+  echo "Missing required DOCKER_USER and/or DOCKER_PASS environment variables"
+  exit
+fi  
+
+# Log in to Docker Hub
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 if [ ! -z "$1" ]; then
   build "$1"
