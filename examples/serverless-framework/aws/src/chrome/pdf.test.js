dafd55b72d509e4d74dd8f5a6c284c48cb2a7c27
Marco Lüthy <marco@luethy.net>
2017-11-17 17:29:16 +0100

    lint and testing improvements

--- examples/serverless-framework/aws/src/chrome/pdf.test.js
+++ examples/serverless-framework/aws/src/chrome/pdf.test.js
@@ -1,37 +1,12 @@
 import test from 'ava'
-import handler from '../handlers/pdf'
 import pdf from './pdf'
 
 const testUrl = 'https://github.com/adieuadieu'
-const testEvent = {
-  queryStringParameters: { url: testUrl },
-}
-
-test('PDF handler', async (t) => {
-  const promise = handler(testEvent, {})
-
-  t.notThrows(promise)
-
-  const result = await promise
-
-  t.is(result.statusCode, 200)
-})
-
-test('PDF handler should throw an error when not provided with a valid URL', async (t) => {
-  const promise = handler({})
-
-  t.throws(promise)
-
-  await promise
-})
 
 test('printUrlToPdf() should return base64 encoded application/pdf', async (t) => {
-  const promise = pdf(testUrl)
+  await t.notThrows(async () => {
+    const result = await pdf(testUrl)
 
-  t.notThrows(promise)
-
-  const result = await promise
-
-  t.is(typeof result, 'string')
-  // TODO: any more assertions we can make here to be assured that we got an actual image?
+    t.is(typeof result, 'string')
+  })
 })
--- examples/serverless-framework/aws/src/chrome/screenshot.test.js
+++ examples/serverless-framework/aws/src/chrome/screenshot.test.js
@@ -1,37 +1,12 @@
 import test from 'ava'
-import handler from '../handlers/screenshot'
 import screenshot from './screenshot'
 
 const testUrl = 'https://github.com/adieuadieu'
-const testEvent = {
-  queryStringParameters: { url: testUrl },
-}
-
-test('Screenshot handler', async (t) => {
-  const promise = handler(testEvent, {})
-
-  t.notThrows(promise)
-
-  const result = await promise
-
-  t.is(result.statusCode, 200)
-})
-
-test('Screenshot handler should throw an error when not provided with a valid URL', async (t) => {
-  const promise = handler({})
-
-  t.throws(promise)
-
-  await promise
-})
 
 test('screenshot() should return base64 encoded image/png', async (t) => {
-  const promise = screenshot(testUrl)
+  await t.notThrows(async () => {
+    const result = await screenshot(testUrl)
 
-  t.notThrows(promise)
-
-  const result = await promise
-
-  t.is(typeof result, 'string')
-  // TODO: any more assertions we can make here to be assured that we got an actual image?
+    t.is(typeof result, 'string')
+  })
 })
