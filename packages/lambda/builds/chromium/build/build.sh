bbc11ae66a44141668334ddf240f5d84435a0613
John Bolliger <johnbolliger@gmail.com>
2019-10-23 01:29:32 -0600

    test for updated yum packages

--- packages/lambda/builds/chromium/build/build.sh
+++ packages/lambda/builds/chromium/build/build.sh
@@ -21,19 +21,23 @@ VERSION=${VERSION:-master}
 printf "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8" >> /etc/environment
 
 # install dependencies
+yum groupinstall "Development Tools"
 yum install epel-release -y
+# yum install -y \
+#   git redhat-lsb python bzip2 tar pkgconfig atk-devel \
+#   alsa-lib-devel bison binutils brlapi-devel bluez-libs-devel \
+#   bzip2-devel cairo-devel cups-devel dbus-devel dbus-glib-devel \
+#   expat-devel fontconfig-devel freetype-devel gcc-c++ GConf2-devel \
+#   glib2-devel glibc gperf glib2-devel gtk2-devel gtk3-devel \
+#   java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel \
+#   libgcc libgnome-keyring-devel libjpeg-devel libstdc++ \
+#   libX11-devel libXScrnSaver-devel libXtst-devel \
+#   libxkbcommon-x11-devel ncurses-compat-libs nspr-devel nss-devel \
+#   pam-devel pango-devel pciutils-devel pulseaudio-libs-devel \
+#   zlib zlib-devel httpd mod_ssl php php-cli python-psutil wdiff --enablerepo=epel
+
 yum install -y \
-  git redhat-lsb python bzip2 tar pkgconfig atk-devel \
-  alsa-lib-devel bison binutils brlapi-devel bluez-libs-devel \
-  bzip2-devel cairo-devel cups-devel dbus-devel dbus-glib-devel \
-  expat-devel fontconfig-devel freetype-devel gcc-c++ GConf2-devel \
-  glib2-devel glibc.i686 gperf glib2-devel gtk2-devel gtk3-devel \
-  java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel \
-  libgcc.i686 libgnome-keyring-devel libjpeg-devel libstdc++.i686 \
-  libX11-devel libXScrnSaver-devel libXtst-devel \
-  libxkbcommon-x11-devel ncurses-compat-libs nspr-devel nss-devel \
-  pam-devel pango-devel pciutils-devel pulseaudio-libs-devel \
-  zlib.i686 httpd mod_ssl php php-cli python-psutil wdiff --enablerepo=epel
+  alsa-lib-devel atk-devel binutils bison bluez-libs-devel brlapi-devel bzip2 bzip2-devel cairo-devel cmake cups-devel dbus-devel dbus-glib-devel expat-devel fontconfig-devel freetype-devel gcc-c++ git glib2-devel glibc gperf gtk3-devel htop httpd java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel libgcc libgnome-keyring-devel libjpeg-devel libstdc++ libuuid-devel libX11-devel libxkbcommon-x11-devel libXScrnSaver-devel libXtst-devel mercurial mod_ssl ncurses-compat-libs nspr-devel nss-devel pam-devel pango-devel pciutils-devel php php-cli pkgconfig pulseaudio-libs-devel python tar zlib zlib-devel --enablerepo=epel
 
 mkdir -p build/chromium
 
