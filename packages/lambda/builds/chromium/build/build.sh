1567e3af1cdac711728ccea77991dc2894fba506
John Bolliger <johnbolliger@gmail.com>
2019-10-23 13:19:43 -0600

    added -y flag for @Development Tools

--- packages/lambda/builds/chromium/build/build.sh
+++ packages/lambda/builds/chromium/build/build.sh
@@ -34,7 +34,7 @@ printf "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8" >> /etc/environment
 #   libxkbcommon-x11-devel ncurses-compat-libs nspr-devel nss-devel \
 #   pam-devel pango-devel pciutils-devel pulseaudio-libs-devel \
 #   zlib zlib-devel httpd mod_ssl php php-cli python-psutil wdiff --enablerepo=epel
-yum groupinstall "Development Tools"
+yum groupinstall -y "Development Tools"
 yum install -y \
   alsa-lib-devel atk-devel binutils bison bluez-libs-devel brlapi-devel bzip2 bzip2-devel cairo-devel cmake cups-devel dbus-devel dbus-glib-devel expat-devel fontconfig-devel freetype-devel gcc-c++ git glib2-devel glibc gperf gtk3-devel htop httpd java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel libgcc libgnome-keyring-devel libjpeg-devel libstdc++ libuuid-devel libX11-devel libxkbcommon-x11-devel libXScrnSaver-devel libXtst-devel mercurial mod_ssl ncurses-compat-libs nspr-devel nss-devel pam-devel pango-devel pciutils-devel php php-cli pkgconfig pulseaudio-libs-devel python tar zlib zlib-devel
 
