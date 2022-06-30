e0277d85c6cf25e99eb85c53aae3bf86e2e4a138
Vetle Leinonen-Roeim <vetle@roeim.net>
2022-06-30 19:07:43 +0000

    Update build image to amazonlinux:2022.0.20220504.1-with-sources and fix dependencies

--- packages/lambda/builds/chromium/build/build.sh
+++ packages/lambda/builds/chromium/build/build.sh
@@ -26,8 +26,8 @@ yum install -y \
   alsa-lib-devel atk-devel binutils bison bluez-libs-devel brlapi-devel \
   bzip2 bzip2-devel cairo-devel cmake cups-devel dbus-devel dbus-glib-devel \
   expat-devel fontconfig-devel freetype-devel gcc-c++ git glib2-devel glibc \
-  gperf gtk3-devel htop httpd java-1.*.0-openjdk-devel libatomic libcap-devel \
-  libffi-devel libgcc libgnome-keyring-devel libjpeg-devel libstdc++ libuuid-devel \
+  gperf gtk3-devel httpd java-11-openjdk-devel libatomic libcap-devel \
+  libffi-devel libgcc libjpeg-devel libstdc++ libuuid-devel \
   libX11-devel libxkbcommon-x11-devel libXScrnSaver-devel libXtst-devel mercurial \
   mod_ssl ncurses-compat-libs nspr-devel nss-devel pam-devel pango-devel \
   pciutils-devel php php-cli pkgconfig pulseaudio-libs-devel python python3 \
